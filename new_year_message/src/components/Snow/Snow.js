cf2f2b3779cc5a7d2f72e3bbd81eaa8d20d2f03c
lywon725 <lywon725@naver.com>
2022-11-15 23:44:02 +0900

    눈 애니메이션 추가
    layout 만들어서 적용

---
+++ new_year_message/src/components/Snow/Snow.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+import "../../styles/snow_effect.css"
+
+const Back = styled.div`
+opacity: 50;
+`
+function Snow() {
+    return (
+        <>
+        <div class="snow1"></div>
+        <div class="snow2"></div>
+        <div class="snow3"></div>
+        </>
+    )
+}
+
+export default Snow
