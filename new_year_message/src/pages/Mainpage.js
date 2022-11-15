5f1613eb61143f5e3223e0d8d86d432707dc5bbe
lywon725 <lywon725@naver.com>
2022-11-15 20:09:34 +0900

    버튼 오류 수정
    버튼 구현 및 페이지 연결

--- new_year_message/src/pages/Mainpage.js
+++ new_year_message/src/pages/Mainpage.js
@@ -15,12 +15,36 @@ padding-top: 30px;
 padding-left: 26px;
 
 `
+const BigButton = styled.button`
+background: #FFFFFF;
+border: 1px solid #B9ADFF;
+box-shadow: 0px 0px 15px rgba(106, 138, 255, 0.1);
+border-radius: 18px;
 
+width: 90%;
+height: 50px;
+position: fixed;
+bottom: 0;
+margin-bottom: 100px;
+
+//텍스트
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 17px;
+line-height: 12px;
+color: #8571FF;
+
+`
 function Mainpage() {
     return (
         <>
-        <Text>보다 따뜻한 2023년을 위해, <br/>응원의 메시지를 <br/>작성해주세요 🍀</Text>
-        </>
+        <Text>
+            보다 따뜻한 2023년을 위해, 
+            <br/>응원의 메시지를 
+            <br/>작성해주세요 🍀
+        </Text>
+        <BigButton onClick={() => (window.location.href = '/nickname')}>작성하러 가기</BigButton>        </>
     )
 }
 
--- new_year_message/src/pages/Title.js
+++ new_year_message/src/pages/Title.js
@@ -15,11 +15,33 @@ padding-top: 30px;
 padding-left: 26px;
 
 `
+const BigButton = styled.button`
+background: #FFFFFF;
+border: 1px solid #B9ADFF;
+box-shadow: 0px 0px 15px rgba(106, 138, 255, 0.1);
+border-radius: 18px;
+
+width: 90%;
+height: 50px;
+position: fixed;
+bottom: 0;
+margin-bottom: 100px;
+
+//텍스트
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 17px;
+line-height: 12px;
+color: #8571FF;
+
+`
 
 function Mainpage() {
     return (
         <>
-        <Text>작성하실 글의 제목을 입력해주세요</Text>
+        <Text>작성하실 글의 <br/>제목을 입력해주세요</Text>
+        <BigButton onClick={() => (window.location.href = '/category')}>다음</BigButton>
         </>
     )
 }
