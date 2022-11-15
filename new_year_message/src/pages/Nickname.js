4bb3344220c13e047d8fc926c1523aad2ea03ca9
lywon725 <lywon725@naver.com>
2022-11-15 19:12:43 +0900

    라우터 추가
    라우터 추가 및 페이지 생성

---
+++ new_year_message/src/pages/Nickname.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Text = styled.h1`
+position: left;
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 600;
+font-size: 22px;
+line-height: 30px;
+letter-spacing: 0.035em;
+
+color: #8571FF;
+padding-top: 30px;
+padding-left: 26px;
+
+`
+
+function Mainpage() {
+    return (
+        <>
+        <Text>닉네임을 입력해주세요</Text>
+        </>
+    )
+}
+
+export default Mainpage
