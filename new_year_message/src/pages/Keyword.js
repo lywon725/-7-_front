9124e6c6e75f6a55594968d5412e84f547815bb7
lywon725 <lywon725@naver.com>
2022-11-17 02:54:21 +0900

    redux 렌더링 안되게
    onClick 수정

--- new_year_message/src/pages/Keyword.js
+++ new_year_message/src/pages/Keyword.js
@@ -1,17 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components';
 import * as S from '../styles/Majorcss';
+//리덕스
+import {useSelector, useDispatch} from "react-redux"
+import {addText, ChangeState} from '../redux/store'
+import { Navigate } from "react-router-dom";
 
 
 function Mainpage() {
     const [inputvalue, setinputvalue] = useState('')
+    //리덕스
+    let a = useSelector((state) => { return state } )
+    let dispatch = useDispatch()
 
+     //렌더링이 완료될때마다 is_done false로 만들어주기
+    useEffect(() => {
+        dispatch(ChangeState())
+    });
     return (
         <S.Wrapper>
         <S.Text>키워드를 활용하여 <br/>메시지를 작성해주세요</S.Text>
-        <S.Put value = {inputvalue} type="text" placeholder="닉네임" onChange={(event)=>setinputvalue(event.target.value)}/>
+        <S.Put value = {inputvalue} type="text" placeholder="메시지" onChange={(event)=>setinputvalue(event.target.value)}/>
         <S.InputLine/>
-        <S.BigButton onClick={() => (window.location.href = '/email')}>다음</S.BigButton>
+        <S.BigButton onClick={() => (dispatch(addText(inputvalue)))}>다음</S.BigButton>
+        {a.user.is_done && (<Navigate to="/email"/> )}  
         </S.Wrapper>
     )
 }
--- new_year_message/src/pages/MCheck.js
+++ new_year_message/src/pages/MCheck.js
@@ -1,14 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components';
 import * as S from '../styles/Majorcss';
 
+//리덕스
+import {useSelector, useDispatch} from "react-redux"
+import {addNULL, ChangeState} from '../redux/store'
+import { Navigate } from "react-router-dom";
+
 
 function MCheck() {
+    //리덕스
+    let a = useSelector((state) => { return state } )
+    let dispatch = useDispatch()
+
+     //렌더링이 완료될때마다 is_done false로 만들어주기
+    useEffect(() => {
+        dispatch(ChangeState())
+    });
     return (
         <S.Wrapper>
         <S.Text>메시지 확인까지 <br/>13시간 12분 15초 남았어요!</S.Text>
-        <S.BigButton onClick={() => (window.location.href = '/MShow')}>확인하러 가기</S.BigButton>
-
+        <S.BigButton onClick={() => (dispatch(addNULL("웅")))}>확인하러 가기</S.BigButton>
+            {a.user.is_done && (<Navigate to="/MShow"/> )} 
         </S.Wrapper>
     )
 }
